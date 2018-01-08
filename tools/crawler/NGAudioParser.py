cba72c2e6ef4f6f5b082946b8ff92820b7e3a4df
Mord3rca <morderca@morderca.net>
2018-01-08 16:29:04 +0100

    [ADD] Crawler: Additionnal web error control [ADD] Crawler: Parsing song's tag [ADD] Crawler: Parsing Validation

--- tools/crawler/NGAudioParser.py
+++ tools/crawler/NGAudioParser.py
@@ -1,82 +1,109 @@
 import http.client, urllib
-import json, sys
+import json, sys, re
 
 sys.path.append("../../")
 from niceFormat import niceuri
 
 class NGAudioParser:
     
-    NG_NAME = "www.newgrounds.com"
+    NG_HOSTNAME  = "www.newgrounds.com"
     NG_AUDIO_URI = "/audio/listen/"
     
-    def __init__(self, id):
-        self.processed = False
+    RE_DATEFORMAT = re.compile("\d{4}/\d{2}/\d{2}")
 
-        self.id = id
+    def __init__(self):
+        self.errorStr = "Obj not run."
+
         self.score = -1
         self.title = ""
         self.composer = ""
-        self.genre = "None"
+        self.genre = "Error"
         self.date = "2003/01/01"
         self.url = ""
+        self.tags = []
 
-    def run(self):
-        print( "[*] Fetching: " + self.NG_AUDIO_URI + str( self.id ) )
-        
-        conn = http.client.HTTPSConnection( self.NG_NAME )
-        conn.request("GET", self.NG_AUDIO_URI + str( self.id ))
-
-        response = conn.getresponse()
+    def run(self, response):
         if response.status != 200:
             print("[-] Status errror. Reason: " + response.reason)
             response.close()
             return
 
-        while True:
-            line = str( response.readline() )
+        data, buff = ["","."*100]
 
-            if line != "":
-                try:
-                    self.__processLine(line)
-                except:
-                    print( "[-] Error while parsing sond ID: " + str( self.id ) )
-                    break
+        while len(buff) >= 100:
+            buff = response.read(100)
+            data += buff.decode("utf-8")
+        
+        data = data.split('\n')
+
+        for line in data:
+            if line.find("id=\"score_number\"") > 0:
+                pos1, pos2 = [ line.find('>'), line.find('<', 15) ]
+                self.score = line[pos1+1:pos2]
+            elif line.find("\"params\":") > 0:
+                #get the embedded player JSON config
+                pos1, pos2 = [ line.find("\"params\":") + len( "\"params\":" ),
+                               line.find("\"icon\":") - 1 ]
+                line = line[pos1:pos2] + '}'
                 
-                if self.genre != "None":
-                    break
+                #Just read it and get the data !
+                jsonObj = json.loads(line)
+                self.composer = urllib.parse.unquote( jsonObj["artist"] )
+                self.url = niceuri( jsonObj["filename"] )
+                self.title = urllib.parse.unquote( jsonObj["name"] )
+            elif line.find("itemprop=\"datePublished\"") > 0:
+                #Get date and format it for the SWF app.
+                pos = line.find("content=\"") + len("content=\"")
+                year, month, day = line[pos:pos+10].split('-')
+                self.date = line[pos:pos+10].replace('-', '/')
+            elif line.find("/audio/browse/genre/") > 0:
+                pos1,pos2 = [line.find(">", 10), line.find('<', 10)]
+                self.genre = line[pos1+1:pos2]
+            elif line.find("/audio/browse/tag/") > 0:
+                pos1,pos2 = [line.find(">", 10), line.find('<', 10)]
+                self.tags.append( line[pos1+1:pos2] )
         
         response.close()
-        self.processed = True
+        
+        return self.__validate()
+    
+    def __validate(self):
+        if not self.RE_DATEFORMAT.match( self.date ):
+            self.errorStr = "[NGParser] Date parsing failed."
+            return False
 
-    def __processLine(self, line):
-        if line.find("id=\"score_number\"") > 0:
-            pos1, pos2 = [ line.find('>'), line.find('<', 15) ]
-            self.score = line[pos1+1:pos2]
-        elif line.find("\"params\":") > 0:
-            #get the embedded player JSON config
-            pos1, pos2 = [ line.find("\"params\":") + len( "\"params\":" ),
-                           line.find("\"icon\":") - 1 ]
-            line = line[pos1:pos2] + '}'
-            
-            #Just read it and get the data !
-            jsonObj = json.loads(line)
-            self.composer = urllib.parse.unquote( jsonObj["artist"] )
-            self.url = niceuri( jsonObj["filename"] )
-            self.title = urllib.parse.unquote( jsonObj["name"] )
-        elif line.find("itemprop=\"datePublished\"") > 0:
-            #Get date and format it for the SWF app.
-            pos = line.find("content=\"") + len("content=\"")
-            year, month, day = line[pos:pos+10].split('-')
-            self.date = line[pos:pos+10].replace('-', '/')
-        elif line.find("/audio/browse/genre/") > 0:
-            pos1,pos2 = [line.find(">", 10), line.find('<', 10)]
-            self.genre = line[pos1+1:pos2]
-            
+        if ( float( self.score ) > 5 or float( self.score ) < 0 ):
+            self.errorStr = "[NGParser] Score not in range [0-5]"
+            return False
 
+        #TODO: PUT Genre verification here.
+        if self.genre == "Error":
+            self.errorStr = "[NGParser] Genre parsing error."
+            return False
+        
+        return True
+
+    def print(self):
+        #print( "id: " + str(self.id) )
+        print( "Title: " + str(self.title))
+        print( "Composer: " + str(self.composer) )
+        print( "Score: " + str( self.score ) )
+        print( "Date: " + self.date )
+        print( "Genre: " + self.genre )
+        print( "Tags: ", self.tags)
+
+#Just for testing
 if __name__ == "__main__":
     from random import randint
-    obj = NGAudioParser( 100000 + randint( 0, 99999 ) )
-    obj.run()
-    if obj.processed:
-        print(obj.composer)
-        print( obj.url )
+    obj, conn = [NGAudioParser(), http.client.HTTPSConnection("www.newgrounds.com")]
+    objid = str( randint( 0, 99999 ) )
+    #objid = "594742"
+    conn.request("GET", obj.NG_AUDIO_URI + objid )
+    check = obj.run( conn.getresponse() )
+
+    print( "ID: " + objid )
+    if not check:
+        print( obj.errorStr )
+    else:
+        obj.print()
+
--- tools/crawler/crawler.py
+++ tools/crawler/crawler.py
@@ -1,54 +1,42 @@
 import sqlite3 as lite
-import time
+import time, sys
 from NGAudioParser import NGAudioParser
-from threading import Thread, Lock
-
-mutex = Lock()
-con = ""
-
-def work(begin, end):
-    local = []
-    for i in range( begin, end ):
-        obj = NGAudioParser(i)
-        obj.run()
-        if( obj.processed ):
-            local.append( tuple( [obj.id, obj.composer, obj.title, obj.score, obj.genre, obj.date, obj.url] ) )
-
-        if i%50 == 0:
-            print("Sleeping a bit to avoid NG overflow...")
-            time.sleep(15)
-
-        if len(local) > 50 or i == base+100000-1:
-            global con,mutex
-            print("Inserting data in DB...")
-            mutex.acquire()
-            con.cursor().executemany("INSERT INTO Songs VALUES(?, ?, ?, ?, ?, ?, ?)", tuple(local))
-            con.commit()
-            local = []
-            mutex.release()
+from http.client import HTTPSConnection
 
 if __name__ == "__main__":
     print("[*] Openning DB...")
-    con = lite.connect("./NG-audio-bridge.sqlite3", check_same_thread=False)
+    dbcon = lite.connect("./NG-audio-bridge.sqlite3", check_same_thread=False)
+    dbcon.cursor().execute("CREATE TABLE IF NOT EXISTS Songs( Id INT PRIMARY KEY not null, composer STRING, title STRING, score FLOAT, genre STRING, date DATE, url STRING, tags STRING);")
+    dbcon.commit()
 
     local = []
-    for x in range(532,750001):
-        obj = NGAudioParser(x)
-        obj.run()
-        if( obj.processed ):
-            local.append( tuple( [obj.id, obj.composer, obj.title, obj.score, obj.genre, obj.date, obj.url] ) )
+    for x in range(1, 250000):
+        resp = None
+        for v in range(0,3):
+            webconn = HTTPSConnection(NGAudioParser.NG_HOSTNAME)
+            webconn.request("GET", NGAudioParser.NG_AUDIO_URI + str(x))
+            resp = webconn.getresponse()
+            if resp.status == 429:
+                print("Sleeping...")
+                time.sleep(10)
+            else:
+                break
 
-        if x%50 == 0:
-            print("Sleeping a bit to avoid NG overflow...")
-            time.sleep(15)
+        if resp.status == 200:
+            obj = NGAudioParser()
+            if( obj.run( resp ) ):
+                local.append( tuple( [x, obj.composer, obj.title, obj.score, obj.genre, obj.date, obj.url, ';'.join(obj.tags)] ) )
+                print("[+] ID: " + str(x) + " - Parsed Successfully")
+            else:
+                print( "ID: " + str(x), "\t" + obj.errorStr, file=sys.stderr )
+        else:
+            print("[-] ID: " + str(x) + " - Status code error (" + str( resp.status ) + ")")
 
-        if len(local) > 50 or x == 750000:
+        if len(local) >= 250 or x == 509:
             print("Inserting data in DB...")
-            #mutex.acquire()
-            con.cursor().executemany("INSERT INTO Songs VALUES(?, ?, ?, ?, ?, ?, ?)", tuple(local))
-            con.commit()
+            dbcon.cursor().executemany("INSERT INTO Songs VALUES(?, ?, ?, ?, ?, ?, ?, ?)", tuple(local))
+            dbcon.commit()
             local = []
-            #mutex.release()
 
     print("[*] Jobs finished. Good Bye.")
-    con.close()
+    dbcon.close()

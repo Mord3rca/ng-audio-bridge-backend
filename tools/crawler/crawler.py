c2e0a7701fdadbc08e44fe8a37260968bb6e4b7a
Mord3rca <morderca@morderca.net>
2018-01-08 23:18:22 +0100

    [ADD] Crawler: 5xx error code support.

--- tools/crawler/crawler.py
+++ tools/crawler/crawler.py
@@ -4,21 +4,30 @@ from NGAudioParser import NGAudioParser
 from http.client import HTTPSConnection
 
 if __name__ == "__main__":
+    count_error_serverside = 0
     print("[*] Openning DB...")
     dbcon = lite.connect("./NG-audio-bridge.sqlite3", check_same_thread=False)
     dbcon.cursor().execute("CREATE TABLE IF NOT EXISTS Songs( Id INT PRIMARY KEY not null, composer STRING, title STRING, score FLOAT, genre STRING, date DATE, url STRING, tags STRING);")
     dbcon.commit()
 
     local = []
-    for x in range(1, 250000):
+    for x in range(12403, 250001):
+    #for x in [11138]:
         resp = None
-        for v in range(0,3):
+        for v in range(0,5):
             webconn = HTTPSConnection(NGAudioParser.NG_HOSTNAME)
             webconn.request("GET", NGAudioParser.NG_AUDIO_URI + str(x))
             resp = webconn.getresponse()
             if resp.status == 429:
-                print("Sleeping...")
+                print("[*] Sleeping...")
                 time.sleep(10)
+            elif resp.status / 100 == 5:
+                print("[*] Server Error Detected ... Long sleep [can solve the problem.]")
+                count_error_serverside += 1
+                if( count_error_serverside >= 5 ):
+                    print("[-] Too much error. Exiting")
+                    exit(-1)
+                time.sleep(15 * 60) #Just sleep 15mn and hope.
             else:
                 break
 
@@ -32,7 +41,7 @@ if __name__ == "__main__":
         else:
             print("[-] ID: " + str(x) + " - Status code error (" + str( resp.status ) + ")")
 
-        if len(local) >= 250 or x == 509:
+        if len(local) >= 250 or x == 250000:
             print("Inserting data in DB...")
             dbcon.cursor().executemany("INSERT INTO Songs VALUES(?, ?, ?, ?, ?, ?, ?, ?)", tuple(local))
             dbcon.commit()

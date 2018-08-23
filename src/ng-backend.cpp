19b8e66b884e2598eb9a8a0f0c318c2eca361be1
Mord3rca <morderca@morderca.net>
2018-08-23 19:26:31 +0200

    [CHG] Moving args stuff in main file [DEL] Removing group option

--- src/ng-backend.cpp
+++ src/ng-backend.cpp
@@ -19,6 +19,99 @@ extern "C"
 AudioServer   *Server = nullptr;
 AudioDatabase *db     = nullptr;
 
+static struct {
+  bool isDaemon = false;
+  std::string user = "nobody";
+  
+  std::string ip = "0.0.0.0";
+  unsigned int port = 8080;
+  
+  std::string db_path;
+  bool isLive = false;
+} args;
+
+void printHelp(std::ostream& out, const char* name) noexcept
+{
+  out << "Usage: " << (name != nullptr ? name : "ng-audio-backend") << " [options]" << std::endl << std::endl
+      << "Options:" << std::endl
+      << "--help, -h" << std::endl
+      << "\tPrint this help message." << std::endl
+      << "--daemonize, -B" << std::endl
+      << "\tRun this program as a service." << std::endl
+      << "--user, -u <username>" << std::endl
+      << "\tRun this program as another user. (default: nobody)" << std::endl
+      << "--group, -g <groupname>" << std::endl
+      << "\tRun this program as another group. (default: nobody)" << std::endl
+      << "--dbpath, -d <filename>" << std::endl
+      << "\tSet the path of the DB file to use." << std::endl
+      << "--live, -s" << std::endl
+      << "\tDo not copy DB in RAM (default: false)" << std::endl
+      << "--socket, -s <ip4:socket>" << std::endl
+      << "\tSet the listening socket. (default: 0.0.0.0:8080)" << std::endl;
+      
+  std::exit( 0 );
+}
+
+static const char *short_option = static_cast<const char*>("hBu:d:ls:");
+static const struct option long_options[] = {
+  {"help",      no_argument,        0, 'h'},
+  {"deamonize", no_argument,        0, 'B'},
+  {"user",      required_argument,  0, 'u'},
+  {"dbpath",    required_argument,  0, 'd'},
+  {"live",      no_argument,        0, 'l'},
+  {"socket",    required_argument,  0, 's'},
+  {0, 0, 0, 0}
+};
+
+static void parse_args(int argc, char **argv)
+{
+  int option_index = 0; int c;
+  
+  while( true )
+  {
+    c = getopt_long(argc, argv, short_option, long_options, &option_index );
+    
+    if( c == -1 )
+      break;
+    
+    switch( c )
+    {
+      case 'h':
+        printHelp(std::cout, argv[0]);
+        break;
+      
+      case 'B':
+        args.isDaemon = true;
+        break;
+      
+      case 'u':
+        args.user = optarg;
+        break;
+      
+      case 'd':
+        args.db_path = optarg;
+        break;
+      
+      case 's':
+      {
+        std::string tmp(optarg); std::string::size_type pos = tmp.find(':');
+        if( pos == std::string::npos )
+        {
+          std::cerr << "[-] Invalid socket format" << std::endl;
+          printHelp(std::cerr, argv[0]);
+        }
+        args.ip    = tmp.substr(0, pos);
+        args.port  = std::stoi( tmp.substr(pos +1) );
+        break;
+      }
+      
+      default:
+        printHelp(std::cerr, argv[0]);
+        break;
+    }
+  }
+}
+
 //You really have to check your privilege !
 static void drop_privilege(const char* user)
 {
@@ -68,26 +161,24 @@ static void signal_handler( int sig )
   }
 }
 
-static void create_objects_via_args(int argc, char **argv)
+static void create_objects_via_args()
 {
-  cArg argument(argc, argv);
-  
   db = new AudioDatabase();
-  if( !db->openDBFile(argument.getDBPath(), argument.isLive()) )
+  if( !db->openDBFile(args.db_path, args.isLive) )
   {
     std::cerr << "Can't open DB File. Exiting..." << std::endl;
     std::exit(-1);
   }
   
-  Server = new AudioServer( argument.getListenIP(),
-                            argument.getListeningPort());
+  Server = new AudioServer( args.ip,
+                            args.port);
   Server->setDBController(db);
   
-  std::cout << "Server listenning on " << argument.getListenIP() << ":" << argument.getListeningPort() << std::endl;
+  std::cout << "Server listenning on " << args.ip << ":" << args.port << std::endl;
   
-  if( argument.daemonize() )
+  if( args.isDaemon )
   {
-    drop_privilege( argument.getUserName().c_str() );
+    drop_privilege( args.user.c_str() );
     daemon(0, 0);
   }
 }
@@ -96,7 +187,8 @@ int main( int argc, char *argv[] )
 {
   signal(SIGINT, signal_handler);
   
-  create_objects_via_args(argc, argv);
+  parse_args(argc, argv);
+  create_objects_via_args();
   
   std::cout << "Entering server loop..." << std::endl;
   Server->serve();

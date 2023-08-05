679a0b9fb09ab74b49adeff73870c52c5db6bcfe
Mord3rca <morderca@morderca.net>
2023-08-05 21:27:43 +0200

    Use %Q instead of '%q'

--- src/filter.cpp
+++ src/filter.cpp
@@ -229,7 +229,7 @@ bool APIFilterComposer::validate() const noexcept {
 const std::string APIFilterComposer::getQuery() const noexcept {
     std::string result; char *buff;
     buff = sqlite3_mprintf("SELECT id,title,composer,score,genre,submission_date,url "
-                           "FROM Tracks WHERE composer LIKE '%q';", m_composer.c_str());
+                           "FROM Tracks WHERE composer LIKE %Q;", m_composer.c_str());
     result = buff;
     sqlite3_free(buff);
     return result;

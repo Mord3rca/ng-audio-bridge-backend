1c3603a5cddde75574e50a21ac30a02ddbd817f8
Mord3rca <morderca@morderca.net>
2023-08-06 16:19:12 +0200

    Write methods for SongItem

--- src/songItem.hpp
+++ src/songItem.hpp
@@ -50,23 +50,40 @@ class SongItem {
     bool operator==(const SongItem& s) const noexcept
     {return s.id() == this->id();}
 
+    void id(unsigned int id)
+    {m_id = id;}
     const unsigned int id() const noexcept
     {return m_id;}
 
+    void title(const std::string &title)
+    {m_title = title;}
     const std::string& title() const noexcept
     {return m_title;}
+
+    void composer(const std::string &composer)
+    {m_composerName = composer;}
     const std::string& composer() const noexcept
     {return m_composerName;}
 
+    void score(const float score)
+    {m_score = score;}
     const float score() const noexcept
     {return m_score;}
 
+    void genre(const enum genre genre)
+    {m_genre = genre;}
+    void genre(const std::string &genre)
+    {m_genre = strToGenre(genre);}
     const enum genre& genre() const noexcept
     {return m_genre;}
 
+    void date(const std::string &date)
+    {m_date = date;}
     const std::string& date() const noexcept
     {return m_date;}
 
+    void url(const std::string &url)
+    {m_path = url;}
     const std::string& url() const noexcept
     {return m_path;}
 
